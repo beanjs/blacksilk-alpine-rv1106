477ec66d322d0ffd7a46bb1112d769354f52dcfe
beanjs <502554248@qq.com>
2024-06-08 09:06:43 +0000

    build alpine rootfs

---
+++ tools/rootfs/alpine/build_in_docker.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+
+# docker run --rm --privileged multiarch/qemu-user-static --reset --persistent yes
+# docker run --rm -it --platform linux/arm/v7 -v ${root}:/home alpine:3.19
+
+root=$(dirname $(realpath $0))
+
+# 切换源
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+
+# 安装基础包
+apk update
+apk add --no-cache openrc openntpd agetty alpine-conf openssh util-linux bash bash-completion e2fsprogs-extra
+apk add --no-cache wpa_supplicant gzip
+# apk add --no-cache apk-tools zfs
+apk cache clean
+
+# 设置root密码
+echo root:password|chpasswd
+
+# 复制启动项
+cp ${root}/patch/etc/init.d/* /etc/init.d
+
+# 添加启动项
+rc-update add cgroups boot
+rc-update add devfs boot
+rc-update add procfs boot
+rc-update add sysfs boot
+rc-update add diskresize default
+rc-update add networking default
+rc-update add sshd default
+
+# 创建导出目录
+mkdir -p ${root}/.rootfs && rm -rf ${root}/.rootfs/*
+
+# 准备文件系统
+for d in bin etc lib media sbin usr var; do tar c "$d" | tar x -C ${root}/.rootfs; done
+for dir in dev opt proc root run sys tmp; do mkdir -p ${root}/.rootfs/${dir}; done
+
+# 合并修改项
+echo merge patch
+cp -rf ${root}/patch/* ${root}/.rootfs
+rm -rf ${root}/.rootfs/var/cache/apk/*
+
+# 导出文件系统
+echo export rootfs
+cd ${root}/.rootfs/ && tar czf rootfs_alpine.tar.gz *
+
+# 退出
+# exit
